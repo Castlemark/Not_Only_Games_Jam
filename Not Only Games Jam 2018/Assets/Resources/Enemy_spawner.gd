27ce54e07bfe6526fd6b02be01131bbeca607783
Sònia Arroyo <sonia.arroyo.2015@salleurl.edu>
2018-05-12 16:55:59 +0200

    Enemies

--- Not Only Games Jam 2018/Assets/Resources/Enemy_spawner.gd
+++ Not Only Games Jam 2018/Assets/Resources/Enemy_spawner.gd
@@ -6,20 +6,26 @@ var enemy2 = preload("res://Assets/Resources/Enemy2.tscn")
 var enemy3 = preload("res://Assets/Resources/Enemy3.tscn")
 
 func _ready():
-	
 	enemies.push_back(enemy1)
 	enemies.push_back(enemy2)
 	enemies.push_back(enemy3)
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
 	
 #	if(enemy.position.y > -5):
 #		enemy = enemy1.instance()
 #		enemy.position.y = -1080
 #		self.add_child(enemy)
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	
+	if(true):
+		var random = randi()%enemies.size()
+		
+		var enemy = enemies[random].instance()
+		var random_x = randi()%1921
+		enemy.position = Vector2 (random_x, -50)
+		self.add_child(enemy)
+	
+	for item in self.get_children():
+		if(item.position.y > 1080):
+			self.remove_child(item)
+			return
