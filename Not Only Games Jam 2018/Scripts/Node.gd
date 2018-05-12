b8df50a80f1cfdf3cbcb3aa561a30928ab8fcd0f
Sònia Arroyo <sonia.arroyo.2015@salleurl.edu>
2018-05-12 15:26:53 +0200

    arreglat merge

--- Not Only Games Jam 2018/Scripts/Node.gd
+++ Not Only Games Jam 2018/Scripts/Node.gd
@@ -8,17 +8,9 @@ var merge
 var time_start = 0
 var time_now = 0
 var time_elapsed
-var enemies = []
-var enemy1 = preload("res://Assets/Resources/Enemy1.tscn")
-var enemy2 = preload("res://Assets/Resources/Enemy2.tscn")
-var enemy3 = preload("res://Assets/Resources/Enemy3.tscn")
-
 
 func _ready():
 	pass
-	enemies.push_back(enemy1)
-	enemies.push_back(enemy2)
-	enemies.push_back(enemy3)
 #	enemy = enemy1.instance()
 #	self.add_child(enemy)
 
@@ -27,9 +19,4 @@ func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here
 	
-#
-#	if(enemy.position.y > -5):
-#		enemy = enemy1.instance()
-#		enemy.position.y = -1080
-#		self.add_child(enemy)
 	
