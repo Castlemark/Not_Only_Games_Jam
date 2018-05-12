1a9e0c6c5ee1b5031269b42ad526566f2dfc91c5
Sònia Arroyo <sonia.arroyo.2015@salleurl.edu>
2018-05-12 15:15:52 +0200

    Auto stash before rebase of "origin/master"

--- Not Only Games Jam 2018/Scripts/Node.gd
+++ Not Only Games Jam 2018/Scripts/Node.gd
@@ -15,27 +15,21 @@ var enemy3 = preload("res://Assets/Resources/Enemy3.tscn")
 
 
 func _ready():
+	pass
 	enemies.push_back(enemy1)
 	enemies.push_back(enemy2)
 	enemies.push_back(enemy3)
-	
-	
-	enemy = enemy1.instance()
-	self.add_child(enemy)
+#	enemy = enemy1.instance()
+#	self.add_child(enemy)
 
 func _process(delta):
+	pass
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here
 	
-	
-	if(enemy.position.y > -5):
-		enemy = enemy1.instance()
-		enemy.position.y = -1080
-		self.add_child(enemy)
-
-	
-	for item in self.get_children():
-		if( item.position.y > 1080):
-			self.remove_child(item)
-			return
+#
+#	if(enemy.position.y > -5):
+#		enemy = enemy1.instance()
+#		enemy.position.y = -1080
+#		self.add_child(enemy)
 	
