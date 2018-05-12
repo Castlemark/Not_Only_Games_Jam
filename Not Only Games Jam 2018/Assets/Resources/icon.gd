de2009e1e09ddc933cd69052c7dd4f6a99d47093
maduixa <genisenco@gmail.com>
2018-05-12 16:04:37 +0200

    Intento de arreglar esta caca

--- Not Only Games Jam 2018/Assets/Resources/icon.gd
+++ Not Only Games Jam 2018/Assets/Resources/icon.gd
@@ -1,7 +1,8 @@
 extends Sprite
 
-var speed = 5
-var target = self.position
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
 
 
 func _ready():
@@ -11,21 +12,4 @@ func _ready():
 
 func _process(delta):
 	
-	self.position += Vector2(0, 1)
-	movePlayer()
 	pass
-	
-func _input(event):
-	if (Input.is_action_pressed("mouse_pressed")):
-		target = event.position
-		
-		
-func movePlayer ():
-	if (target != null):
-		var dir = Vector2(target - self.position)
-	
-		if (dir.length() >= 3): 
-			dir = dir.normalized()
-			self.position += speed * dir
-		else:
-			target = null
