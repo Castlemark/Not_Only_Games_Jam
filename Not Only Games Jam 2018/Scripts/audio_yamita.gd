66d0152b4bdc013efe1434f1a3d9e9357fcf3ba1
Sònia Arroyo <sonia.arroyo.2015@salleurl.edu>
2018-05-13 05:34:40 +0200

    audio

---
+++ Not Only Games Jam 2018/Scripts/audio_yamita.gd
@@ -0,0 +1,22 @@
+extends AudioStreamPlayer2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var time = 0
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	self.play()
+	pass
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	
+	time += delta
+	
+	if (time > 2):
+		self.stop()
+	pass
