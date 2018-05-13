7e109de14053ffb3faf504d1ebd3bfc07e57b8d9
maduixa <genisenco@gmail.com>
2018-05-13 07:22:14 +0200

    Salt completat

--- Not Only Games Jam 2018/Scripts/KinematicBody2D.gd
+++ Not Only Games Jam 2018/Scripts/KinematicBody2D.gd
@@ -6,6 +6,8 @@ var move = 0
 var type = 1;
 var cam;
 var sentit = 1;
+var char_scene = preload("res://Assets/Resources/character.tscn")
+var mouse_pos
 # tipus
 #	1: + velocitat
 #	2: - velocitat
@@ -42,6 +44,23 @@ func _ready():
 	pass
 
 func _process(delta):
+	for body_inside in bodies:
+		if ("character" in body_inside.get_parent().name):
+			if (Vector2(mouse_pos - (Vector2(body_inside.global_position + Vector2(0,-50)))).length() < 100 ):
+				print("Helo 3")
+				var aux = char_scene.instance()
+				print(body_inside.get_parent().name)
+				for item in aux.get_children():
+					if(item.name == "KinematicBody2D"):
+						aux.remove_child(item)
+				aux.copyInfo(body_inside.get_parent())
+				self.remove_child(get_node("icon"))
+				aux.name = 'icon'
+				var move = body_inside.get_parent().position - self.get_parent().position
+				body_inside.get_parent().get_parent().remove_child(body_inside.get_parent())
+				self.move_and_collide(to_local(move))
+				self.add_child(aux)
+			
 	move = Vector2(0, 1)
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
@@ -58,14 +77,17 @@ func _process(delta):
 	
 	
 func _input(event):
+	
 	if (Input.is_action_pressed("mouse_pressed")):
+		mouse_pos = event.position
 		if (cam.current):			
 			cam.zoom = Vector2(1,1)
 			target = event.position
 			cam.zoom = Vector2(0.5,0.5)
-			print(target)
 		else:
 			target = event.position
+	if( Input.is_action_just_released("mouse_pressed")):
+		mouse_pos = Vector2( 10000,1000)
 		
 func movePlayer ():
 	if (target != null):
@@ -79,3 +101,13 @@ func movePlayer ():
 
 func getPosition():
 	return position
+
+var bodies = []
+func _on_Area2D_body_entered(body):
+	bodies.append(body)
+	
+
+
+func _on_Area2D_body_exited(body):
+	bodies.erase(body)
+	pass # replace with function body
